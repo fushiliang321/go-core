b44b904b2f90d07ae0156ddb64be00d39c70e0dd
代码搬运工 <994301536@qq.com>
2022-09-19 10:19:56 +0800

    新增 rpc客户端结构体

--- rpc/client/client.go
+++ rpc/client/client.go
@@ -8,6 +8,32 @@ import (
 	"log"
 )
 
+type Client struct {
+	serverName      string
+	serverNameSnake string
+}
+
+func New(server string) *Client {
+	return &Client{
+		serverName:      server,
+		serverNameSnake: helper.SnakeString(server),
+	}
+}
+
+func (c *Client) Call(method string, params any, res any) (err error) {
+	defer func() {
+		exception.Listener("rpc call", recover())
+	}()
+	rpcClient, err := newRpcClient(c.serverName)
+	if err == nil {
+		err = rpcClient.Call(c.serverNameSnake+"/"+method, params, res, false)
+		if err != nil {
+			log.Println("rpcClient error", err)
+		}
+	}
+	return
+}
+
 func newRpcClient(name string) (jsonrpc.ClientInterface, error) {
 	node, err := consul.GetNode(name + "Service")
 	if err != nil {
@@ -18,15 +44,5 @@ func newRpcClient(name string) (jsonrpc.ClientInterface, error) {
 }
 
 func Call(server string, method string, params any, res any) (err error) {
-	defer func() {
-		exception.Listener("rpc call", recover())
-	}()
-	rpcClient, err := newRpcClient(server)
-	if err == nil {
-		err = rpcClient.Call(helper.SnakeString(server)+"/"+method, params, res, false)
-		if err != nil {
-			log.Println("rpcClient error", err)
-		}
-	}
-	return
+	return New(server).Call(method, params, res)
 }
