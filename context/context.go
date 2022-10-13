257e5fae14e6c59564da2bb56cf14c55106380fb
代码搬运工 <994301536@qq.com>
2022-10-13 17:56:49 +0800

    完善 协程上下文数据操作包

--- context/context.go
+++ context/context.go
@@ -4,6 +4,17 @@ import "github.com/timandy/routine"
 
 var local = routine.NewInheritableThreadLocal()
 
+// 获取全部协程上下文数据
+func GetAll() map[string]any {
+	data := local.Get()
+	if data != nil {
+		if data, ok := data.(map[string]any); ok {
+			return data
+		}
+	}
+	return nil
+}
+
 // 获取协程上下文数据
 func Get(key string) any {
 	data := local.Get()
@@ -27,6 +38,21 @@ func Set(key string, value any) {
 		}
 	}
 	_map[key] = value
+	local.Set(_map)
+}
+
+// 批量设置协程上下文数据
+func SetBatch(values map[string]any) {
+	_map := map[string]any{}
+	data := local.Get()
+	if data != nil {
+		if data, ok := data.(map[string]any); ok {
+			_map = data
+		}
+	}
+	for key, value := range values {
+		_map[key] = value
+	}
 	local.Set(_map)
 }
 
