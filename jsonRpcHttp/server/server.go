71dd09e92f5b2d67dc1173a6d1b1b307fe90d910
代码搬运工 <994301536@qq.com>
2022-10-18 17:39:34 +0800

    新增 jsonrpchttp服务支持接收请求上下文数据

---
+++ jsonRpcHttp/server/server.go
@@ -0,0 +1,52 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/fushiliang321/go-core/jsonRpcHttp/context"
+	"github.com/iloveswift/go-jsonrpc/common"
+	"reflect"
+)
+
+type Server struct {
+	common.Server
+}
+
+func (svr *Server) Handler(b []byte) []byte {
+	fmt.Println("Handler")
+	data, err := common.ParseRequestBody(b)
+	if err != nil {
+		return jsonE(nil, common.JsonRpc, common.ParseError)
+	}
+	var res interface{}
+	if reflect.ValueOf(data).Kind() == reflect.Slice {
+		var resList []interface{}
+		for _, v := range data.([]interface{}) {
+			r := svr.SingleHandler(v.(map[string]interface{}))
+			resList = append(resList, r)
+		}
+		res = resList
+	} else if reflect.ValueOf(data).Kind() == reflect.Map {
+		r := svr.SingleHandler(data.(map[string]interface{}))
+		res = r
+	} else {
+		return jsonE(nil, common.JsonRpc, common.InvalidRequest)
+	}
+
+	response, _ := json.Marshal(res)
+	return response
+}
+
+func (svr *Server) SingleHandler(jsonMap map[string]interface{}) interface{} {
+	if ctx, ok := jsonMap["context"]; ok {
+		if ctx, ok := ctx.(map[string]any); ok {
+			context.SetBatch(ctx)
+		}
+	}
+	return svr.Server.SingleHandler(jsonMap)
+}
+
+func jsonE(id interface{}, jsonRpc string, errCode int) []byte {
+	e, _ := json.Marshal(common.E(id, jsonRpc, errCode))
+	return e
+}
