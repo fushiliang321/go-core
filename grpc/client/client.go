5f81c926d8b85fac354d9acd8d7083802db04e89
代码搬运工 <994301536@qq.com>
2022-11-21 09:35:17 +0800

    调整 grpc客户端会返回错误信息

--- grpc/client/client.go
+++ grpc/client/client.go
@@ -7,7 +7,7 @@ import (
 	"reflect"
 )
 
-type ClientGenerateFun[t any] func(isMultiplex ...bool) t
+type ClientGenerateFun[t any] func(isMultiplex ...bool) (t, error)
 
 var multiplexConns = map[any]*ClientConn{}
 
@@ -18,7 +18,7 @@ func init() {
 }
 
 func NewClient[t any](serviceName string, fun func(cc grpc.ClientConnInterface) t) ClientGenerateFun[t] {
-	return func(isMultiplex ...bool) t {
+	return func(isMultiplex ...bool) (t, error) {
 		defer func() {
 			exception.Listener("grpc call exception:", recover())
 		}()
@@ -30,13 +30,14 @@ func NewClient[t any](serviceName string, fun func(cc grpc.ClientConnInterface) 
 		var client t
 		if err != nil {
 			exception.Listener("grpc newClient Error:["+serviceName+"]", err)
+			return nil, err
 		} else {
 			client = fun(conn)
 			if multiplex {
 				multiplexConns[client] = conn
 			}
 		}
-		return client
+		return client, nil
 	}
 }
 
