dc7e7f182a754599b4d6242c329563001f0d4d49
代码搬运工 <994301536@qq.com>
2023-03-11 14:59:31 +0800

    优化

--- jsonRpcHttp/server/health.go
+++ jsonRpcHttp/server/health.go
@@ -10,30 +10,25 @@ type Health struct{}
 var (
 	resultSuccess = "success"
 	resultError   = "error"
+	internalErr   = common.NewInternalErr(resultError, ErrorResponse{
+		Code: fasthttp.StatusGone,
+		Text: resultError,
+	})
 )
 
 func (s *Health) Check(params *registerInfo) (*string, error) {
 	if len(serviceRegistrations) == 0 {
 		//没有注册的服务
-		return &resultError, common.NewInternalErr(resultError, ErrorResponse{
-			Code: fasthttp.StatusGone,
-			Text: resultError,
-		})
+		return &resultError, internalErr
 	}
 	registration, ok := serviceRegistrations[params.Name]
 	if !ok {
 		//检测的服务没有注册
-		return &resultError, common.NewInternalErr(resultError, ErrorResponse{
-			Code: fasthttp.StatusGone,
-			Text: resultError,
-		})
+		return &resultError, internalErr
 	}
 	if registration.Protocol != params.Protocol {
 		//服务的协议不一致
-		return &resultError, common.NewInternalErr(resultError, ErrorResponse{
-			Code: fasthttp.StatusGone,
-			Text: resultError,
-		})
+		return &resultError, internalErr
 	}
 	return &resultSuccess, nil
 }
