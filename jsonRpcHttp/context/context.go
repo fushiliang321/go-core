ceb72f46dfc1f450b80443c75024303857dc96d6
代码搬运工 <994301536@qq.com>
2023-03-21 18:41:52 +0800

    调整 jsonrpc中的context替换为context模块的下的helper

--- jsonRpcHttp/context/context.go
+++ jsonRpcHttp/context/context.go
@@ -2,29 +2,27 @@ package context
 
 import "github.com/fushiliang321/go-core/context"
 
-var local = context.NewInstance()
-
 // 获取全部协程上下文数据
 func GetAll() map[string]any {
-	return local.GetAll()
+	return context.GetAll()
 }
 
 // 获取协程上下文数据
 func Get(key string) any {
-	return local.Get(key)
+	return context.Get(key)
 }
 
 // 设置协程上下文数据
 func Set(key string, value any) {
-	local.Set(key, value)
+	context.Set(key, value)
 }
 
 // 批量设置协程上下文数据
 func SetBatch(values map[string]any) {
-	local.SetBatch(values)
+	context.SetBatch(values)
 }
 
 // 移除协程上下文数据
 func Remove(key string) {
-	local.Remove(key)
+	context.Remove(key)
 }
