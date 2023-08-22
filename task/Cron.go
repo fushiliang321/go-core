bac9e291ab98504eec4fee1aff245aa7469acb77
代码搬运工 <994301536@qq.com>
2023-08-22 15:39:44 +0800

    调整 task相关方法，新增Cron结构体

---
+++ task/Cron.go
@@ -0,0 +1,64 @@
+package task
+
+import "github.com/robfig/cron"
+
+type (
+	Cron struct {
+		before func(sign any)
+		after  func(sign any)
+		*cron.Cron
+	}
+
+	Job struct {
+		cmd  func()
+		sign any
+	}
+	Schedule = cron.Schedule
+)
+
+func (j Job) Run() {
+	j.cmd()
+}
+
+func (c *Cron) AddFuncSign(spec string, cmd func(), sign any) error {
+	return c.AddJob(spec, Job{cmd: cmd, sign: sign})
+}
+
+func (c *Cron) AddFunc(spec string, cmd func()) error {
+	return c.AddJob(spec, Job{cmd: cmd})
+}
+
+func (c *Cron) AddJob(spec string, cmd Job) error {
+	schedule, err := cron.Parse(spec)
+	if err != nil {
+		return err
+	}
+	c.Schedule(schedule, cmd)
+	return nil
+}
+
+func (c *Cron) Schedule(schedule Schedule, cmd Job) {
+	_cmd := cmd.cmd
+	if c.before != nil {
+		if c.after != nil {
+			cmd.cmd = func() {
+				c.before(cmd.sign)
+				_cmd()
+				c.after(cmd.sign)
+			}
+		} else {
+			cmd.cmd = func() {
+				c.before(cmd.sign)
+				_cmd()
+			}
+		}
+	} else if c.after != nil {
+		cmd.cmd = func() {
+			_cmd()
+			c.after(cmd.sign)
+		}
+	} else {
+		_cmd = nil
+	}
+	c.Cron.Schedule(schedule, cmd)
+}
--- task/main.go
+++ task/main.go
@@ -3,7 +3,6 @@ package task
 import (
 	"github.com/fushiliang321/go-core/config/task"
 	"github.com/fushiliang321/go-core/event"
-	"github.com/robfig/cron"
 	"sync"
 )
 
@@ -16,25 +15,29 @@ func (*Service) Start(wg *sync.WaitGroup) {
 	}
 	wg.Add(1)
 	go func(wg *sync.WaitGroup) {
-		defer wg.Done()
+		defer func() {
+			recover()
+			wg.Done()
+		}()
+		cron := &Cron{
+			before: func(sign any) {
+				event.NewRegistered(event.BeforeTaskExecute, sign)
+			},
+			after: func(sign any) {
+				event.NewRegistered(event.AfterTaskExecute, sign)
+			},
+		}
 		event.Dispatch(event.NewRegistered(event.BeforeTaskServerStart, nil))
-		c := cron.New()
 		for i := range config.Crontabs {
 			crontab := config.Crontabs[i]
 			callback := crontab.Callback
-			BeforeTaskExecuteRegistered := event.NewRegistered(event.BeforeTaskExecute, crontab)
-			AfterTaskExecuteRegistered := event.NewRegistered(event.AfterTaskExecute, crontab)
-			err := c.AddFunc(crontab.Rule, func() {
-				event.Dispatch(BeforeTaskExecuteRegistered)
-				callback()
-				event.Dispatch(AfterTaskExecuteRegistered)
-			})
+			err := cron.AddFuncSign(crontab.Rule, callback, crontab)
 			if err != nil {
 				continue
 			}
 			event.Dispatch(event.NewRegistered(event.TaskRegister, crontab))
 		}
 		event.Dispatch(event.NewRegistered(event.AfterTaskServerStart, nil))
-		c.Run()
+		cron.Run()
 	}(wg)
 }
