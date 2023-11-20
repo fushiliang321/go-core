8cc5ce911de5ee8806fc11c3473039f129db82de
代码搬运工 <994301536@qq.com>
2023-11-20 15:24:38 +0800

    优化 对每个服务启动的异常进行捕获并抛出异常信息，单个服务异常不再导致整个进程无法启动

--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
 	"github.com/fushiliang321/go-core/event"
 	"github.com/fushiliang321/go-core/exception"
 	"github.com/fushiliang321/go-core/helper/logger"
+	"reflect"
 	"sync"
 )
 
@@ -24,7 +25,15 @@ func Start() {
 		wg := &sync.WaitGroup{}
 		servers := initialize.Get()
 		for _, ser := range servers {
-			ser.Start(wg)
+			func(s initialize.Service) {
+				defer func() {
+					if err := recover(); err != nil {
+						logger.Error("core start error:", reflect.ValueOf(s).Elem().Type().String(), fmt.Sprint(err))
+						exception.Listener("core start error", err)
+					}
+				}()
+				s.Start(wg)
+			}(ser)
 		}
 		event.Dispatch(event.NewRegistered(event.AfterServerStart))
 		wg.Wait()
