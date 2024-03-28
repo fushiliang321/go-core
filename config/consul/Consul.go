53478af93ae6821c41e82fc0c9afce347936d264
代码搬运工 <994301536@qq.com>
2024-03-28 15:33:36 +0800

    调整 ConsulServiceHealthCheck结构体重命名为HealthCheck

--- config/consul/Consul.go
+++ config/consul/Consul.go
@@ -5,19 +5,19 @@ import (
 )
 
 type (
-	ConsulServiceHealthCheck struct {
-		Timeout                        string //健康检测超时时间
+	HealthCheck struct {
+		Timeout                        string // 健康检测超时时间
 		Interval                       string // 健康检查间隔
 		DeregisterCriticalServiceAfter string // check失败后删除服务，注销时间，相当于过期时间
 	}
 	Consul struct {
 		api.Config
-		HealthCheck *ConsulServiceHealthCheck
+		HealthCheck *HealthCheck
 	}
 )
 
 var consul = &Consul{
-	HealthCheck: &ConsulServiceHealthCheck{},
+	HealthCheck: &HealthCheck{},
 }
 
 func Set(config *Consul) {
